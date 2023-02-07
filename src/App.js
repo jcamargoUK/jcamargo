2665f6b34cc82c564888e16d28528c62fc9f2544
Julio Camargo <jcamargo@hotmail.co.uk>
2023-02-07 14:54:07 +0000

    added footer and header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
 function App() {
   return (
+    <>
+    <Header />
     <h1>Under Construction</h1>
+    <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const footer = () => {
+  return (
+    <footer>
+      <p>Created by JCamargo, © 2019</p>
+    </footer>
+  )
+}
+export default footer
